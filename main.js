c8cfcb69f5b3f4b1b14f8d28820e2d749ed679bc
Dave89dc <127298096+Dave89dc@users.noreply.github.com>
2023-04-24 17:15:36 +0200

    my-canvas

--- main.js
+++ main.js
@@ -8,29 +8,55 @@ const point = {
 
 // RETTANGOLO:
 
-cxt.rect(point.x, point.y, 100, 100);
-cxt.lineWidth = 10;
-cxt.strokeStyle = 'red';
-cxt.stroke();
+// cxt.rect(point.x, point.y, 100, 100);
+// cxt.lineWidth = 10;
+// cxt.strokeStyle = 'red';
+// cxt.stroke();
 
-cxt.fillStyle = 'blueviolet';
-cxt.fillRect(point.x, point.y, 100, 100);
+// cxt.fillStyle = 'blueviolet';
+// cxt.fillRect(point.x, point.y, 100, 100);
 
-cxt.rect(100, 100, 100, 100);
-cxt.lineWidth = 10;
-cxt.strokeStyle = 'red';
-cxt.stroke();
+// cxt.rect(100, 100, 100, 100);
+// cxt.lineWidth = 10;
+// cxt.strokeStyle = 'red';
+// cxt.stroke();
 
-cxt.fillStyle = 'blueviolet';
-cxt.fillRect(point.x, point.y, 100, 100);
-cxt.fill();
+// cxt.fillStyle = 'blueviolet';
+// cxt.fillRect(point.x, point.y, 100, 100);
+// cxt.fill();
 
 // CERCHIO:
 
-// cxt.arc(point.x, point.y, 60, 0, 2*Math.PI);
-// cxt.lineWidth = 4;
-// cxt.strokeStyle = 'red';
-// cxt.stroke();
+// let x = Math.random()*myCanvas.width;
+// let y = Math.random()*myCanvas.height;
+// let r = Math.random()*100;
+
+let pointUpX = 400;
+let pointUpY = 350;
+let pointDownX = 400;
+let pointDownY = 450;
+
+function step(){
+    cxt.fillStyle = 'rgb(215, 215, 215)';
+    cxt.fillRect(0, 0, myCanvas.width, myCanvas.height);
+    cxt.fillStyle = 'white';
+    cxt.fillRect(290, 380, 220, 20);
+    cxt.beginPath();
+    generateRandom();
+    cxt.arc(point.x, point.y, 100, 0, 2*Math.PI);
+    point.x += (Math.random()*2)-1;
+    point.y += (Math.random()*2)-1;
+    cxt.lineWidth = 2;
+    cxt.strokeStyle = 'blue';
+    cxt.stroke();
+    cxt.fillStyle = 'rgb(30, 144, 255)';
+    cxt.fill();
+    
+    
+    window.requestAnimationFrame(step);
+};
+
+window.requestAnimationFrame(step);
 
 // cxt.beginPath();
 // cxt.arc(100, 100, 50, 0, 2 * Math.PI);
@@ -50,4 +76,38 @@ cxt.fill();
 // cxt.fill();
 // cxt.strokeStyle = "#000000";
 // cxt.stroke();
+
+
+let speedX = (Math.random()*18)-9;
+
+function generateRandom(){
+
+    cxt.fillStyle = `rgb(${Math.random()*255}, 0, 0)`;
+    cxt.fillRect(pointUpX, pointUpY, 30, 4);
+
+    cxt.fillStyle = `rgb(${Math.random()*255}, 0, 0)`;
+    cxt.fillRect(pointDownX, pointDownY, 30, 4);
+
+    let speedUpY = -6;
+    let speedDownY = 6;
+
+    if(pointUpY < 0){
+        speedX = (Math.random()*18)-9;
+        pointUpX = 400;
+        pointUpY = 350;
+    };
+
+    if(pointDownY > myCanvas.height){
+        speedX = (Math.random()*18)-9;
+        pointDownX = 400;
+        pointDownY = 450;
+    };
+
+    pointUpX += speedX;
+    pointUpY += speedUpY;
+
+    pointDownX += speedX;
+    pointDownY += speedDownY;
+
+};
 
--- point.js
+++ point.js
@@ -1,24 +1,44 @@
 class Point{
 
-    constructor(x, y, speedX, speedY, color){
+    constructor(x, y, speedY, color){
         this.x = x;
         this.y = y;
-        this.speedX = speedX;
         this.speedY = speedY;
         this.color = color;
     };
 
     draw(ctx){
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, 10, 10);
+        ctx.fillRect(this.x, this.y, 6, 6);
     };
 
-    static generatePoint(canvasWidth){
+    static generatePointUp(canvasWidth){
 
         const color = `rgb(150, 0, 20)`;
 
-        const randomX = Math.random()*canvasWidth;
-        const  
+        const randomX1 = Math.random()*canvasWidth;
+        const Y1 = 0;
+
+        const speedY1 = (Math.random()*2)-1;
+
+        const newPoint1 = new Point(randomX1, Y1, speedY1, color);
+
+        return newPoint1;
+
+    };
+
+    static generatePointDown(canvasWidth){
+
+        const color = `rgb(150, 0, 20)`;
+
+        const randomX2 = Math.random()*canvasWidth;
+        const Y2 = 800;
+
+        const speedY2 = (Math.random()*-2)+1;
+
+        const newPoint2 = new Point(randomX2, Y2, speedY2, color);
+
+        return newPoint2;
 
     };
 
